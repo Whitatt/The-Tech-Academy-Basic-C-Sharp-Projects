193ae197c35d13a8dd14200e760134e466d8ed41
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-13 16:27:02 -0800

    update

---
+++ DailySubmission.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"{min} to {max}");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
