873576defda05dbdc0b412410df8c5c5edd6c59b
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-16 18:33:46 -0800

    update

---
+++ Mathand ComparisonOperators/Mathand ComparisonOperators/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Mathand_ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;  THIS IS ADDITION
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5; THIS IS SUBTRACTION
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;   THIS IS MULTIPLICATION
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;   THIS IS DIVISION
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;  THIS PROVIDES A REMAINDER, SO IF WE DIVIDIE 11 by 2, there will be 1 number left.
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+            ////bool isWarm = currentTemperature <= roomTemperature;
+            //bool isWarm = currentTemperature != roomTemperature;
+
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            Console.WriteLine("Person1");
+            Console.WriteLine("Hourly Rate");
+            int hourlyRate = 15;
+
+            Console.WriteLine("Hours worked per week?");
+            int hoursWorked = 40;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
