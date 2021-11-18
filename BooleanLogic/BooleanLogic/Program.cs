4c810bb0a1d27b065a9c38af353b088ad6d98067
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-18 12:57:50 -0800

    update

--- BooleanLogic/BooleanLogic/Program.cs
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -29,7 +29,9 @@ namespace BooleanLogic
             Console.WriteLine("What is your age?");
             int ageNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Have you ever had a DUI?");
-
+            bool noDUI = false;
+            bool yesDUI = true;
+            Console.WriteLine
             Console.WriteLine("How many speeding tickets do you have?");
             int speedingTickets = Convert.ToInt32(Console.ReadLine());
 
