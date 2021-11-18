70b0f967c46a758785080aaeaf1b606594fa2d48
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-18 12:36:12 -0800

    update

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false); AND OPERATOR
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true); OR OPERATOR
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true); EQUAL OPERATOR
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true); DOES NOT EQUAL OPERATOR
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true); X OR OPERATOR The result of x ^ y is true if x evaluates to true and y evaluates to false, or x evaluates to false and y evaluates to true.
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.WriteLine("What is your age?");
+            int ageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+        }
+    }
+}
