9b480d804daa3bf8fe4a0d8f2ff592885598ae59
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-21 18:48:36 -0800

    update

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -12,27 +12,43 @@ namespace Loops
         {
             Console.WriteLine("Guess a number?");
             int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
 
-            switch (number)
+
+            do
             {
-                case 62:
-                     Console.WriteLine("You guessed 62. Try again.");
-                     break;
-                case 29:
-                     Console.WriteLine("You guessed 29. Try again.");
-                     break;
-`               case 55:
-                     Console.WriteLine("You guessed 55. Try again.");
-                     break;
-                case 12:
-                     Console.WriteLine("You guessed the number 12. That is correct!");
-                     break;
-                default:
-                     Console.WriteLine("You are wrong.");
-                     break;
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
 
                 }
                 Console.ReadLine();
+            }
+            while (!isGuessed);
+            
             
         }
     }
---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            while (count <= 4)
+            {
+                Console.WriteLine("The value of i is : " + count);
+                count = count + 1;
+            }
+            Console.ReadKey();
+        
+
+        }
+    }
+}
