dc8f337f9e000c468ec0b2d39ac076b7c5d12fdc
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-21 16:00:55 -0800

    update

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -11,12 +11,13 @@ namespace Branching
             Console.WriteLine("Please enter the package weight");
             int packWeight = Convert.ToInt32(Console.ReadLine());
 
-            if (packWeight > 50)
+            if (packWeight > 50)//Determines packweight less than 50
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day" : "Please enter package width");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");//displays outcome if weight less than 50 or over 50
             }
             else
             {
+
                 Console.WriteLine("Please enter the package width:");
                 int packWidth = Convert.ToInt32(Console.ReadLine());
 
@@ -25,6 +26,21 @@ namespace Branching
 
                 Console.WriteLine("Please enter the package length");
                 int packLength = Convert.ToInt32(Console.ReadLine());
+
+                if (packWidth + packHeight + packLength > 50)//adding variables width height length
+                {
+                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");//output if package greater than 50
+                }
+                else
+                {
+                    
+                    decimal quote = Convert.ToDecimal((packWidth * packHeight * packLength * packWeight) / 100);//multiplies width length height and divides by 100
+                    Console.WriteLine($"Your estimated total for shipping this package is ${quote}.00");//curly brackets provide total above multiplication and division
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+
+                
             }
 
            
