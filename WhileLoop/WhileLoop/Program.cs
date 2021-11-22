1904716e9e38609afbd4f8f67295a203df8592b0
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-21 18:54:47 -0800

    update

--- WhileLoop/WhileLoop/Program.cs
+++ WhileLoop/WhileLoop/Program.cs
@@ -6,14 +6,19 @@ namespace WhileLoop
     {
         static void Main(string[] args)
         {
-            int count = 1;
-            while (count <= 4)
             {
-                Console.WriteLine("The value of i is : " + count);
-                count = count + 1;
-            }
-            Console.ReadKey();
-        
+                int i = 1;
+                while (i <= 4)
+                {
+                    Console.WriteLine("i value: {0}", i);
+                    i++;
+                }
+                Console.WriteLine("Press Enter Key to Exit..");
+                Console.ReadLine();
+            }   //executing statement within while loop by checking the condition (i <= 4) and
+                //increasing the variable i (i++)
+                //value to 1 by using the increment operator.
+
 
         }
     }
