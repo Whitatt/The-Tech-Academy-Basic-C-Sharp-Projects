4dbb2a33bb03e2a5404a5bcd4128ce398dcb24c7
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-22 20:44:27 -0800

    update

--- ArraysandLists/ArraysandLists/Program.cs
+++ ArraysandLists/ArraysandLists/Program.cs
@@ -12,7 +12,39 @@ using System.Collections.Generic;
         Books[0] = "Nailbiter";
         Books[1] = "Y The Last Man";
         Books[2] = "Manifest Destiny";
-        
+
+        Console.WriteLine("Please enter a number from 0 to 3");
+        int index1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"This string is at the index you chose: {Books[index1]}");
+
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        Console.WriteLine("Please select a number from 0 to 4");
+        int array1 = Convert.ToInt32(Console.ReadLine());
+
+        if (array1 > 4)
+        {
+            Console.WriteLine("The array does not exist");
+        }
+        else
+        {
+            Console.WriteLine($"This is array is the number you chose: {array[array1]}");
+        }
+
+        var numbers = new List<int>();
+        numbers.Add(2);
+        numbers.Add(3);
+        numbers.Add(5);
+        numbers.Add(7);
+        Console.WriteLine("Please select a number 2, 3, 5, 7");
+        Console.WriteLine("LIST 1: " + numbers.Count);
+        Console.ReadLine();
 
 
 
