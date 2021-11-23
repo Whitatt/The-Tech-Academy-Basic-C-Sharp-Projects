a5ebc7eabcf461dd84dce881bef579cf4633cf29
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-22 18:42:27 -0800

    update

---
+++ ArraysandLists/ArraysandLists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        //Declaring a single dimensional array
+        string[] Books = new string[3];
+        Books[0] = "Nailbiter";
+        Books[1] = "Y The Last Man";
+        Books[2] = "Manifest Destiny";
+        
+
+
+
+
+
+
+        //List<string> intList = new List<string>();
+        //intList.Add("Hello");
+        //intList.Add("Matthew");
+        //intList.Remove("Matthew");
+
+        //Console.WriteLine(intList[0]);
+        //Console.ReadLine();
+
+        //byte[] byteArray = 
+
+                
+        //    int[] numArray = new int[5];
+        //    numArray[0] = 5;
+        //    numArray[1] = 2;
+        //    numArray[2] = 10;
+        //    numArray[3] = 200;
+        //    numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2000 };
+
+        //numArray2[5] = 650;
+
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+        }
+    }
+
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //{
+        //    string name = "Matthew";
+        //    string quote = "The man said, \\Hello\", Matthew. \n Hello on a new line. \n \t Hello on a tab."; ;
+        //    string fileName = @"C:\users\Matthew";
+
+        //    bool trueOrFalse = name.Contains("e");
+        //    trueOrFalse = name.EndsWith("d");
+
+        //    int length = name.Length;
+
+        //    name = name.ToUpper();
+
+
+
+
+        //    Console.WriteLine(name);
+        //    Console.ReadLine();
+
+            string name = "Matthew";
+            name = "Matt";
+
+            Console.WriteLine(name);
+            Cponsole.ReadLine();
+            
+        }
+    }
+}
