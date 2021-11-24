6c5c54535ea09a12d7f05386d22bd6f57bb7fa2a
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-24 15:12:52 -0800

    update

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Cars = new string[4];
+            Cars[0] = "Toyota";
+            Cars[1] = "VW";
+            Cars[2] = "Pontiac";
+            Cars[3] = "AMC";
+
+            Console.WriteLine("Please select a number from 0 to 4");
+        }
+    }
+}
