f5114cf88ae967ace07658917581b37b17ed3aca
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-24 19:35:22 -0800

    update

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -1,30 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace ConsoleAppAssignment
-{
+
     class Program
     {
         static void Main(string[] args)
-        {   //Assignment Part 1
-            string[] Cars = { "Porsche", "Cadillac", "Toyota", "Ford", "Chevrolet" };
+        {
 
-            //opening statement providing user instruction that prints to the console
-            Console.WriteLine("Name one country that makes the vehicles above listed?");
+            //Assignment Part 1
+            string[] Cars = { "Pontiac", "Dodge", "GMC", "Ford", "Chevrolet" };
+
+           //opening statement providing user instruction that prints to the console
+            Console.WriteLine("Name the country that makes the vehicles \n Pontiac, Dodge, GMC, Ford and Cheverolet");
             string response1 = (Console.ReadLine()); //user data entry
 
-            for (int i = 0; < Cars.Length; i++) // loop iterates array to # of index positions
-            {
+            for (int i = 0; i < Cars.Length; i++) // loop iterates array to # of index positions
+           {
                 Cars[i] = Cars[i] + response1; //statement that concatenates original array strings and user input
                 Console.WriteLine("Result = " + Cars[i]); //Updated array of strings that prints to console
-            }
+           }
             Console.ReadLine(); // holds open program for next section of code to run
 
             Console.WriteLine();
 
 
 
-            
-        }
+
     }
 }
+
