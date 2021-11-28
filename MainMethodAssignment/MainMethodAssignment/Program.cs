d4a466eafd00e6298d335ba93fdd6515383ca502
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-28 00:55:45 -0800

    update

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // retrieve # from user and call method addition
+            Console.WriteLine("Please enter a number to add to 7:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Set.Addition(userNumber, 7));
+
+            //retrieve # from user and call method multiply
+            Console.WriteLine("Please enter a decimal number to be multiplied by 8:");
+            decimal userInput = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(Set.Multiply(userInput, 8));
+
+            //retrieve # from user and call method multi
+            Console.WriteLine("Please enter a number to be multiplied by 10:");
+            String userString = Console.ReadLine();
+            int dub = Convert.ToInt32(userString);
+            Console.WriteLine(Set.Multi(dub, 10));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Set.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Set
+    {
+        // Add the user's integer to another integer
+        public static int Addition(int userNumber, int y)
+        {
+            int result = userNumber + y;
+
+            return result;
+        }
+
+        // Multiply the user's decimal by an integer and return an integer as the answer
+        public static decimal Multiply(decimal userInput, int y)
+        {
+            int result = (int)userInput * y;
+
+            return result;
+        }
+
+        // Multiplies the user's input by an integer
+        public static double Multi(int userString, int y)
+        {
+            int result = userString * y;
+
+            return result;
+        }
+    }
+}
