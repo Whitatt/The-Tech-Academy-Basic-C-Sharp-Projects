b3814e3a2ba8c8e47b299959f7613f21604b6b6e
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-28 13:04:58 -0800

    update

---
+++ MethodClass/MethodClass/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Class
+    {
+        // Create method "MyMethod" with two integers as parameters
+        public static void MyMethod(int intOne, int intTwo)
+        {
+            // Multiply first integer by 8
+            int result = intOne * 8;
+            // Display "result"
+            Console.WriteLine($"{intOne} multiplied by 8 = {result}");
+            // Display unused integer
+            Console.WriteLine("Unused integer: " + intTwo);
+            return;
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer: ");
+            // Get first # from user
+            int intOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second integer: ");
+            // Get second # from user
+            int intTwo = Convert.ToInt32(Console.ReadLine());
+
+            // Call method "MyMethod" to display results of user input
+            Class.MyMethod(intOne: intOne, intTwo: intTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
