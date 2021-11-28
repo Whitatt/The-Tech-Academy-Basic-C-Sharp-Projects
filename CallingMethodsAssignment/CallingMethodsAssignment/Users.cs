4216d0e12d39d3126644e457ab3bacac015fc042
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-28 14:33:46 -0800

    updates

--- CallingMethodsAssignment/CallingMethodsAssignment/Users.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Users.cs
@@ -8,19 +8,19 @@ namespace CallingMethodsAssignment
 {
     public class Users
     {
-        public double GetAdd(int data)  //method 1: adds 10 to user integer data entry
+        public int GetAdd(int data)  //method 1: adds 10 to user integer data entry
         {
             return data + 10;  //method operation (addition)
         }
-        public double GetSubtract(int data)  //method 2: subtracts 10 from user integer data entry
+        public int GetSubtract(int data)  //method 2: subtracts 10 from user integer data entry
         {
             return data - 10;  //method operation (subtraction)
         }
-        public double GetMultiply(int data)  //method 3: multiplies user integer data entry by 10
+        public int GetMultiply(int data)  //method 3: multiplies user integer data entry by 10
         {
             return data * 10;  //method operation (multiplication)
         }
-        public double GetDivide(int data)  //method 4: divides user integer data entry by 10
+        public int GetDivide(int data)  //method 4: divides user integer data entry by 10
         {
             return data / 10;  //method operation (division)
         }
