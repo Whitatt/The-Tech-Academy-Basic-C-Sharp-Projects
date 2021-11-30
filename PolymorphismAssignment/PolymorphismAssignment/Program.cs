d664092b60501b16205ef0f465393e9066df5482
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-11-29 23:42:23 -0800

    update

---
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling in the Employee class
+            Employee employee = new Employee();
+
+            //give values to the properties
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            //call sayName() which will use the property values to do its job and display to the console
+            employee.SayName();
+            Console.ReadLine();
+
+            //creating an object of type Iquittable and call the Quit() method
+            IQuittable guy = new Employee();
+
+            //calling on the quit method
+            guy.Quit(employee);
+            Console.ReadLine();
+        }
+    }
+}
