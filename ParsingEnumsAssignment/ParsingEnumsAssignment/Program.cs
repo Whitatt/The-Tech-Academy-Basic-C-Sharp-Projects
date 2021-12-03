91920c685482b53daa570d4993aabc21ffec3657
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-12-02 18:24:20 -0800

    update

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try catch block to prepare for possible user input errors
+            try
+            {
+                //instantiated the ClassWithEnum
+                EnumClass variable = new EnumClass();
+                //Prompt user to enter day of the week
+                Console.WriteLine("Enter the current day of the week:");
+                string userInput = Console.ReadLine();
+                variable.Day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput);
+                Console.WriteLine("Yes that is it");
+            }
+            catch (System.ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week. (example, Monday)");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
